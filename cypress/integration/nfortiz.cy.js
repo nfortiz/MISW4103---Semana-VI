d38212398326c15b70078f0706fee399d2303c69
Fernando Ortiz Suarez <nf.ortiz@uniandes.edu.co>
2024-11-07 04:10:54 +0000

    add wait time

--- cypress/integration/nfortiz.cy.js
+++ cypress/integration/nfortiz.cy.js
@@ -5,7 +5,7 @@ Cypress.on("uncaught:exception", (err, runnable) => {
 });
 
 beforeEach(()=>{
-    cy.visit('http://localhost:8080/ghost/#/signin');
+   cy.visit('http://localhost:8080/ghost/#/signin');
    cy.get('a[title="Dashboard"]').should('be.visible');
 });
 
@@ -13,14 +13,14 @@ it("Create new post", () => {
     cy.get('a[data-test-new-page-button=""]').click(); //Click on New Page
     cy.location("hash").should("equal", "#/editor/post");
 
-    cy.intercept("POST", "/ghost/api/canary/admin/posts/", {}).as("createPost");
 
-    cy.get("textarea.gh-editor-title").click(); // Click en el titulo
-    cy.get("div.koenig-editor__editor-wrapper").click();
+    cy.get('textarea[placeholder="Post title"]').type(title).screenshot("post title")
+    cy.get('div[data-placeholder="Begin writing your post..."]').type(content).screenshot("post body")
 
-    cy.wait("@createPost");
+    cy.wait(500);
     cy.location("hash").should("include", "#/editor/post/");
 
     cy.get("div.gh-publishmenu").click(); // click en publicar
-    cy.get("button#ember96").click(); // Clic en publicar
+    cy.get('div').contains('Publish').click()
+
 });
