19508e0a757ec058ec75dea69b6c302c311a590d
Fernando Ortiz Suarez <nf.ortiz@uniandes.edu.co>
2024-11-08 01:02:10 +0000

    edit page test

--- cypress/integration/nfortiz.cy.js
+++ cypress/integration/nfortiz.cy.js
@@ -54,7 +54,7 @@ it("Create empty page", () => {
 
     cy.wait(500)
 
-    cy.get('button[data-test-button="publish-flow"]').click(); // click en publicar
+    cy.get('button[data-test-button="publish-flow"]').first().click(); // click en publicar
 
     cy.wait(500)
     cy.get('div.epm-modal-container').within(() => {
@@ -69,30 +69,24 @@ it("Create empty page", () => {
 it("Edit page", () => {
     cy.visit(BASE_URL + '/ghost/#/pages/')
     cy.screenshot('Before Edit');
-    cy.get('span.gh-post-list-cta.edit').click(); //Click on New Page
+    cy.get('span.gh-post-list-cta.edit').click(); //Click on Edit first page
     cy.location("hash").should("equal", "#/editor/page"); // check location
 
     cy.intercept("PUT", "/ghost/api/admin/pages/", {}).as("createPage");
 
-    // Set content
+    // Set new content
     cy.get('textarea[data-test-editor-title-input=""]').type("Edited Page")
-    cy.get('div[data-placeholder="Begin writing your post..."]').type(" It is unsafe to chain further commands that rely on the subject afte.")
-
-    cy.get('textarea[data-test-editor-title-input=""]').clear();
-    cy.get('div[data-placeholder="Begin writing your post..."]').clear();    
-
-    cy.wait("@createPage")
-
-    cy.get('button[data-test-button="publish-flow"]').click(); // click en publicar
+    cy.get('p[data-koenig-dnd-droppable="true"]').first().type(" Edited with cypress. by nf.ortiz 😊")
 
     cy.wait(500)
-    cy.get('div.epm-modal-container').within(() => {
-        cy.get('button[data-test-button="continue"]').contains('Publish').click() // click en continuar
-        cy.get('span[data-test-task-button-state="idle"]').click(); //click en confirmar
-    })
+
+    cy.get('span[data-test-task-button-state="idle"]').first().click(); // click en update
 
     cy.wait(500)
-    cy.screenshot('New Page')
+    cy.get('aside.gh-notifications').screenshot("edit notification");
+
+    cy.wait(500)
+    cy.get('a[data-test-link="pages"]').first().click();    
 });
 
 it("Unpublish page", () => {
@@ -154,25 +148,6 @@ it("Delete page", () => {
 });
 
 
-  function createPage(title, content) {
-    cy.visit(BASE_URL + 'ghost/#/pages/')
-    cy.get('a[data-test-new-page-button=""]').click(); 
-    cy.wait(100)
-    cy.get('textarea[placeholder="Post title"]').type(title)
-    cy.get('div[data-placeholder="Begin writing your post..."]').type(content)
-    cy.wait(700)
-    cy.screenshot('filled post creation form')
-    cy.get('div').contains('Publish').click()
-    cy.wait(700)
-    cy.screenshot('filled post creation form 2')
-    cy.get('button').contains('Publish').click()
-    cy.get('.modal-content').within(() => {
-        cy.get('button').contains('Publish').click()
-    })
-    cy.wait(700)
-    cy.screenshot('Publishing')
-    cy.get('.gh-notification-title').should("have.text", "Published")
-    cy.get('a').contains('View Post').invoke('attr', 'href').then((attr) => {+attr}).as('postUrl');
-    cy.screenshot('Published')
-    cy.get('button[class="gh-notification-close"]').click()
+function goToCreatePage() {
+
 }
