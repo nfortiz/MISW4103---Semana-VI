261539960b049d544c820baf7c20a439135720ae
Daniel Andrade Suarez <d.andrades@uniandes.edu.co>
2024-11-10 13:21:00 -0500

    Añadimos patrón Given-When-Then al archivo E00020.

--- cypress/e2e/E00020.cy.js
+++ cypress/e2e/E00020.cy.js
@@ -17,8 +17,8 @@ describe("Escenarios E2E para Ghost", function () {
     LogIn.logInButton();
   });
 
-  it("E0020 - Delete Member", function () {
-    // Crear un nuevo miembro
+  it("E00020 - Delete Member", function () {
+    // When Crear un nuevo miembro
     PrincipalPage.visitMembers(BASE_URL);
     cy.wait(2000);
     MembersPage.getScreenTitle().should("include.text", "Members");
@@ -34,23 +34,22 @@ describe("Escenarios E2E para Ghost", function () {
     MembersPage.fillMemberForm(memberData);
     MembersPage.clickSaveButton();
 
-    // Volver a la lista de miembros y buscar el miembro creado
+    // Then Volver a la lista de miembros y buscar el miembro creado
     MembersPage.goToMembersList();
     cy.wait(2000);
 
-    // Seleccionar el miembro en la lista para editarlo
+    // Then Seleccionar el miembro en la lista para editarlo
     MembersPage.clickMemberByEmail(memberData.email);
 
-    // Abrir el menú de acciones y confirmar eliminación
+    // Then Abrir el menú de acciones y confirmar eliminación
     MembersPage.openMemberActions();
     MembersPage.clickDeleteMember();
     MembersPage.confirmDeleteMember();
 
-    // Verificar que el miembro ya no está en la lista
+    // Then Verificar que el miembro ya no está en la lista
     MembersPage.getMembersList().then((membersList) => {
       const member = membersList.find((m) => m.email === memberData.email);
       expect(member).to.be.undefined;
     });
   });
-
 });
