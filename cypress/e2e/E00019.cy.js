be03301a5a43a0d0920b0f5f2a7294b597b404ea
Daniel Andrade Suarez <d.andrades@uniandes.edu.co>
2024-11-10 13:20:19 -0500

    Añadimos patrón Given-When-Then al archivo E00019.

--- cypress/e2e/E00019.cy.js
+++ cypress/e2e/E00019.cy.js
@@ -17,8 +17,8 @@ describe("Escenarios E2E para Ghost", function () {
     LogIn.logInButton();
   });
 
-  it("E0019 - Edit Member", function () {
-    // Crear un nuevo miembro
+  it("E00019 - Edit Member", function () {
+    // When Crear un nuevo miembro
     PrincipalPage.visitMembers(BASE_URL);
     cy.wait(2000);
     MembersPage.getScreenTitle().should("include.text", "Members");
@@ -34,21 +34,21 @@ describe("Escenarios E2E para Ghost", function () {
     MembersPage.fillMemberForm(initialMemberData);
     MembersPage.clickSaveButton();
 
-    // Volver a la lista de miembros y buscar el miembro creado
+    // Then Volver a la lista de miembros y buscar el miembro creado
     MembersPage.goToMembersList();
     cy.wait(2000);
 
     MembersPage.clickMemberByEmail(initialMemberData.email);
 
-    // Editar el nombre del miembro
+    // Then Editar el nombre del miembro
     const updatedName = faker.name.fullName();
     cy.get('input[data-test-input="member-name"]').clear().type(updatedName);
 
-    // Guardar los cambios
+    // Then Guardar los cambios
     MembersPage.clickSaveButton();
     MembersPage.goToMembersList();
 
-    // Confirmar que el nombre ha cambiado en la lista
+    // Then Confirmar que el nombre ha cambiado en la lista
     cy.contains("p.gh-members-list-email", initialMemberData.email)
       .parent()
       .within(() => {
