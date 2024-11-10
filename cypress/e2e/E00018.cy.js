e84e8e4fb55791a3596fea9e5cdfc6325b869bbd
Daniel Andrade Suarez <d.andrades@uniandes.edu.co>
2024-11-10 13:19:32 -0500

    Añadimos patrón Given-When-Then al archivo E00018.

--- cypress/e2e/E00018.cy.js
+++ cypress/e2e/E00018.cy.js
@@ -23,7 +23,7 @@ describe("Escenarios E2E para Ghost", function () {
   });
 
   it("E0017 - Invalid Email Validation", function () {
-    // Navegar a la sección de miembros y abrir el formulario para crear un nuevo miembro
+    // When Navegar a la sección de miembros y abrir el formulario para crear un nuevo miembro
     PrincipalPage.visitMembers(BASE_URL);
     cy.wait(2000);
     MembersPage.getScreenTitle().should("include.text", "Members");
@@ -37,30 +37,30 @@ describe("Escenarios E2E para Ghost", function () {
         expect(normalizedText).to.include("New member");
       });
 
-    // Generar texto de más de 500 caracteres para el campo de nota
+    // Then Generar texto de más de 500 caracteres para el campo de nota
     const longNote = faker.lorem.words(100); // Genera un texto largo que excede los 500 caracteres
 
-    // Generar un nombre y nota válidos, pero un email inválido
+    // Then Generar un nombre y nota válidos, pero un email inválido
     const memberData = {
       name: faker.name.fullName(),
       email: "invalid-email-format", // Email inválido
       note: longNote,
     };
 
-    // Llenar el formulario con el email inválido
+    // Then Llenar el formulario con el email inválido
     MembersPage.fillMemberForm(memberData);
 
-    // Hacer clic en el botón para guardar el nuevo miembro
+    // Then Hacer clic en el botón para guardar el nuevo miembro
     MembersPage.clickSaveButton();
 
-    // Verificar que se muestre el mensaje de error "Invalid Email."
+    // Then Verificar que se muestre el mensaje de error "Invalid Email."
     MembersPage.getInvalidEmailMessageElement().should(
       "contain.text",
       "Invalid Email."
     );
     // Verificar que no se haya hecho ninguna solicitud POST ni PUT
 
-    // Verificar que el contador de caracteres indique que se ha excedido el límite y esté en rojo
+    // Then Verificar que el contador de caracteres indique que se ha excedido el límite y esté en rojo
     cy.get("span.word-count")
       .should("have.css", "color", "rgb(226, 84, 64)") // Color rojo
       .and(($span) => {
